eab51deeff44e555e694d86e6017f4ecdee48406
Tine <tine@tjo.space>
2024-04-23 09:42:31 +0200

    fix(sunset|sunrise): also apply to vim and vi aliases

--- scripts/sunset.sh
+++ scripts/sunset.sh
@@ -6,7 +6,7 @@ echo "include themes/rose-pine-moon.conf" >$HOME/.config/kitty/theme.conf
 kill -SIGUSR1 $(pidof kitty)
 
 # Neovim
-kill -SIGUSR1 $(pidof nvim)
+kill -SIGUSR1 $(pidof nvim) $(pidof vim) $(pidof vi)
 
 # Bat
 sed -i "s/--theme=.*/--theme=OneHalfDark/g" ${CONFIG_DIR}/bat/config
